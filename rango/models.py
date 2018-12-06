481c70254690a1ac3f2a89385ac638e912cff950
ichinara <vivianelaisdefarias@gmail.com>
2018-12-06 04:51:46 -0200

    Database plural
    5.6 Configuring the Admin Interface

--- rango/models.py
+++ rango/models.py
@@ -6,7 +6,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     
-    def __str__(self): # For Python 2, use __unicode__ too
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self): 
         return self.name
 
 
@@ -16,5 +19,5 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-    def __str__(self): # For Python 2, use __unicode__ too
+    def __str__(self): 
         return self.title
